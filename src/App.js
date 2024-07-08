48508c2cf7ef56e432ab04e7f53a971cbf96a9a6
SamalaNeelima <samalaneelima763@gmail.com>
2024-07-08 23:11:57 +0530

    removed text animation and justified text

--- src/App.js
+++ src/App.js
@@ -5,50 +5,45 @@ import Header from './pages/Header.js';
 function App() {
   return (
     <div className="App">
-     <Header />
-
-      
+      <Header />
       <div className='content'>
-        
-          <h1>Privacy Policy</h1>
-          <p><strong>Effective Date:</strong> 2-07-2024</p>
+        <h1>Privacy Policy</h1>
+        <p><strong>Effective Date:</strong> 2-07-2024</p>
 
-          <h2>Introduction</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque euismod consequat massa, ac facilisis justo scelerisque sed. Integer non libero scelerisque, facilisis quam et, pretium orci. In lacinia nisi nec urna consequat bibendum. Mauris non nulla aliquet, sagittis metus a, aliquet enim. Donec tincidunt libero sit amet turpis posuere, vel ullamcorper lectus dictum. Fusce tristique risus id arcu malesuada, non interdum erat vehicula. Phasellus quis risus vitae sapien dapibus laoreet. Suspendisse potenti. Etiam bibendum nunc sit amet varius dapibus. Duis consectetur, mi vel tempor dictum, arcu massa venenatis nulla, at fermentum orci est in ex.</p>
+        <h2>Introduction</h2>
+        <p className="justify-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque euismod consequat massa, ac facilisis justo scelerisque sed. Integer non libero scelerisque, facilisis quam et, pretium orci. In lacinia nisi nec urna consequat bibendum. Mauris non nulla aliquet, sagittis metus a, aliquet enim. Donec tincidunt libero sit amet turpis posuere, vel ullamcorper lectus dictum. Fusce tristique risus id arcu malesuada, non interdum erat vehicula. Phasellus quis risus vitae sapien dapibus laoreet. Suspendisse potenti. Etiam bibendum nunc sit amet varius dapibus. Duis consectetur, mi vel tempor dictum, arcu massa venenatis nulla, at fermentum orci est in ex.</p>
 
-          <h2>Information Collection</h2>
-          <h3>Personal Data</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vehicula semper nisi, nec tincidunt ligula venenatis et. Vivamus id sapien magna. Aliquam erat volutpat. Nullam facilisis, lorem non fermentum ullamcorper, felis leo dignissim lectus, sit amet facilisis nisi ligula a augue. Ut malesuada libero a nibh vehicula, sit amet viverra mi placerat. Morbi dictum mi ut ligula fringilla, id vestibulum purus malesuada. Integer at dui at lorem pharetra bibendum sit amet a odio. Praesent in varius justo. Etiam sit amet augue sed urna congue sollicitudin. Curabitur auctor dui nec nulla molestie, at tincidunt purus ultrices.</p>
+        <h2>Information Collection</h2>
+        <h3>Personal Data</h3>
+        <p className="justify-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vehicula semper nisi, nec tincidunt ligula venenatis et. Vivamus id sapien magna. Aliquam erat volutpat. Nullam facilisis, lorem non fermentum ullamcorper, felis leo dignissim lectus, sit amet facilisis nisi ligula a augue. Ut malesuada libero a nibh vehicula, sit amet viverra mi placerat. Morbi dictum mi ut ligula fringilla, id vestibulum purus malesuada. Integer at dui at lorem pharetra bibendum sit amet a odio. Praesent in varius justo. Etiam sit amet augue sed urna congue sollicitudin. Curabitur auctor dui nec nulla molestie, at tincidunt purus ultrices.</p>
 
-          <h3>Usage Data</h3>
-          <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+        <h3>Usage Data</h3>
+        <p className="justify-text">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
 
-          <h2>Use of Data</h2>
-          <h3>Providing and Maintaining Service</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. Cras venenatis euismod malesuada. Vestibulum consectetur purus a libero interdum, vitae auctor elit fringilla. Nunc congue mi eget urna dictum, nec luctus velit ullamcorper. Aenean nec tincidunt nisi, sit amet blandit nunc. Nam id magna a erat faucibus sagittis. Aenean malesuada augue vel varius aliquet. Nulla facilisi. Etiam sit amet ante nec velit dignissim pulvinar. Nam sed dui a nibh varius feugiat. Nulla facilisi. Praesent ac orci vel risus tincidunt suscipit. Mauris sit amet sem ac nunc tristique accumsan.</p>
+        <h2>Use of Data</h2>
+        <h3>Providing and Maintaining Service</h3>
+        <p className="justify-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. Cras venenatis euismod malesuada. Vestibulum consectetur purus a libero interdum, vitae auctor elit fringilla. Nunc congue mi eget urna dictum, nec luctus velit ullamcorper. Aenean nec tincidunt nisi, sit amet blandit nunc. Nam id magna a erat faucibus sagittis. Aenean malesuada augue vel varius aliquet. Nulla facilisi. Etiam sit amet ante nec velit dignissim pulvinar. Nam sed dui a nibh varius feugiat. Nulla facilisi. Praesent ac orci vel risus tincidunt suscipit. Mauris sit amet sem ac nunc tristique accumsan.</p>
 
-          <h3>Performance of Contract</h3>
-          <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?</p>
+        <h3>Performance of Contract</h3>
+        <p className="justify-text">Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?</p>
 
-          <h2>Data Transfer</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vehicula nunc quis felis faucibus, vitae molestie nisl hendrerit. Praesent ut eros vel turpis suscipit dictum ac quis augue. Ut sit amet nisl metus. Vivamus ullamcorper, libero ut pulvinar efficitur, nulla orci hendrerit dolor, non posuere odio lorem id arcu. Nam ac turpis dictum, egestas est at, sollicitudin orci. Donec pharetra feugiat mi et imperdiet. Suspendisse potenti. Nulla mollis enim nec enim fermentum, sed accumsan sapien vestibulum. Vestibulum ut dolor et libero bibendum tincidunt ac nec quam. Nam feugiat felis non felis dapibus, in luctus odio scelerisque. Nullam eget justo purus.</p>
+        <h2>Data Transfer</h2>
+        <p className="justify-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vehicula nunc quis felis faucibus, vitae molestie nisl hendrerit. Praesent ut eros vel turpis suscipit dictum ac quis augue. Ut sit amet nisl metus. Vivamus ullamcorper, libero ut pulvinar efficitur, nulla orci hendrerit dolor, non posuere odio lorem id arcu. Nam ac turpis dictum, egestas est at, sollicitudin orci. Donec pharetra feugiat mi et imperdiet. Suspendisse potenti. Nulla mollis enim nec enim fermentum, sed accumsan sapien vestibulum. Vestibulum ut dolor et libero bibendum tincidunt ac nec quam. Nam feugiat felis non felis dapibus, in luctus odio scelerisque. Nullam eget justo purus.</p>
 
-          <h2>Data Security</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus auctor urna in dolor consequat, et dictum neque vehicula. Integer malesuada massa in odio feugiat, at lobortis ante ullamcorper. Ut nec mi ac odio scelerisque faucibus. Aliquam erat volutpat. Sed vitae urna non magna volutpat scelerisque. Fusce fermentum, metus eu ullamcorper venenatis, enim justo fermentum velit, ut viverra quam felis et tortor. Nam eget turpis vitae purus fermentum tristique. Suspendisse potenti. Donec vestibulum tempor sem, nec malesuada nunc ullamcorper eget. Integer in libero dapibus, posuere nisl sed, accumsan sapien. Aenean tincidunt, lorem in condimentum finibus, nulla orci ultricies sem, ac luctus ligula magna a ex. Donec ac elit vitae sapien viverra porttitor.</p>
+        <h2>Data Security</h2>
+        <p className="justify-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus auctor urna in dolor consequat, et dictum neque vehicula. Integer malesuada massa in odio feugiat, at lobortis ante ullamcorper. Ut nec mi ac odio scelerisque faucibus. Aliquam erat volutpat. Sed vitae urna non magna volutpat scelerisque. Fusce fermentum, metus eu ullamcorper venenatis, enim justo fermentum velit, ut viverra quam felis et tortor. Nam eget turpis vitae purus fermentum tristique. Suspendisse potenti. Donec vestibulum tempor sem, nec malesuada nunc ullamcorper eget. Integer in libero dapibus, posuere nisl sed, accumsan sapien. Aenean tincidunt, lorem in condimentum finibus, nulla orci ultricies sem, ac luctus ligula magna a ex. Donec ac elit vitae sapien viverra porttitor.</p>
 
-          <h2>Data Retention</h2>
-          <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Curabitur non felis sem. Aliquam erat volutpat. Maecenas aliquet libero vel lorem scelerisque, sed elementum libero gravida. Nulla facilisi. Mauris vel dui a odio viverra commodo non sed libero. Praesent venenatis, mauris a pharetra facilisis, lorem velit consectetur nunc, ac efficitur leo nisl in ex. Suspendisse potenti.</p>
+        <h2>Data Retention</h2>
+        <p className="justify-text">Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Curabitur non felis sem. Aliquam erat volutpat. Maecenas aliquet libero vel lorem scelerisque, sed elementum libero gravida. Nulla facilisi. Mauris vel dui a odio viverra commodo non sed libero. Praesent venenatis, mauris a pharetra facilisis, lorem velit consectetur nunc, ac efficitur leo nisl in ex. Suspendisse potenti.</p>
 
-          <h2>Disclosure of Data</h2>
-          <h3>Business Transactions</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur non ligula vitae odio fermentum bibendum a ut justo. Suspendisse potenti. Sed sollicitudin, sapien in fermentum sodales, magna arcu vehicula mi, nec dapibus elit justo id orci. Praesent sit amet justo nec erat interdum vehicula. Nam luctus, libero sit amet hendrerit fermentum, arcu metus pretium erat, in mollis odio justo non justo. Fusce consequat, nisi ut aliquam dapibus, justo odio gravida velit, nec vulputate urna mauris sit amet orci. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Quisque mollis odio sed enim ullamcorper lacinia. Nulla auctor quam sit amet risus ullamcorper.</p>
+        <h2>Disclosure of Data</h2>
+        <h3>Business Transactions</h3>
+        <p className="justify-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur non ligula vitae odio fermentum bibendum a ut justo. Suspendisse potenti. Sed sollicitudin, sapien in fermentum sodales, magna arcu vehicula mi, nec dapibus elit justo id orci. Praesent sit amet justo nec erat interdum vehicula. Nam luctus, libero sit amet hendrerit fermentum, arcu metus pretium erat, in mollis odio justo non justo. Fusce consequat, nisi ut aliquam dapibus, justo odio gravida velit, nec vulputate urna mauris sit amet orci. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Quisque mollis odio sed enim ullamcorper lacinia. Nulla auctor quam sit amet risus ullamcorper.</p>
 
-          <h3>Legal Requirements</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum viverra, nisi a fermentum tincidunt, magna libero congue nisl, a facilisis sapien nunc sit amet enim. Suspendisse eget ultricies metus, in faucibus odio. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus nec mi non lacus cursus tincidunt. Curabitur gravida risus ut ligula placerat, et aliquam ligula congue. Aenean malesuada arcu vel metus lacinia, a egestas sem pharetra. Sed auctor auctor nisi, nec pulvinar justo lacinia in. Proin tristique, sapien in lobortis tristique, est orci faucibus nisi, in dictum ipsum odio et purus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
-        </div>
-        </div>
-
-   
+        <h3>Legal Requirements</h3>
+        <p className="justify-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum viverra, nisi a fermentum tincidunt, magna libero congue nisl, a facilisis sapien nunc sit amet enim. Suspendisse eget ultricies metus, in faucibus odio. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus nec mi non lacus cursus tincidunt. Curabitur gravida risus ut ligula placerat, et aliquam ligula congue. Aenean malesuada arcu vel metus lacinia, a egestas sem pharetra. Sed auctor auctor nisi, nec pulvinar justo lacinia in. Proin tristique, sapien in lobortis tristique, est orci faucibus nisi, in dictum ipsum odio et purus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+      </div>
+    </div>
   );
 }
 
